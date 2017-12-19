c8056c60bcfa5ce569a56c93900f13866b3c2362
Sanchyy <sanchez.gassull@gmail.com>
2017-12-20 00:03:42 +0100

    base

--- server.js
+++ server.js
@@ -6,3 +6,28 @@ var index = require('./routes/index'),
 	tasks = require('./routes/tasks');
 
 var app = express();
+
+var port = 3000;
+
+//view engine
+app.set('views',path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html',require('ejs').renderFile);
+
+// Set Static Folder
+
+app.use(express.static(path.join(__dirname, 'client')));
+
+//Body Parser MW
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended:false
+}));
+
+app.use('/', index); //./routes/indexjs
+app.use('/api',tasks); // ./routes/tasks
+
+app.listen(port, function() {
+	console.log('server started on port: ' + port);
+})
